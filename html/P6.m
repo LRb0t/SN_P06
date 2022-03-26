69e4c9858663737f3a4b27dd8d9e86cd1d1b0d17
LRb0t <95806659+LRb0t@users.noreply.github.com>
2022-03-25 23:37:19 -0600

    inicio

---
+++ html/P6.m
@@ -0,0 +1,28 @@
+%% Practica 6
+% Equipo los difusos
+%% Problema 1
+% El problema 1 requiere
+%%
+% 
+% * 2 entradas (servicio y comida)
+% * El universo de discuros de ambas variables lingüisticas es 1 salida (propina)
+% * El universo de discurso de esta variable lingúistica es [0,15] 
+% * Cada variable lingúistica dene tener por lo menos cuatro valores lingúisticos. Por lo menos un valor lingúistico debe ser una variación de un primitivo (por ejemplo: muy rica).
+% * La semantica de cada variable lingüista se deja a su "experiencia como consumidor"
+% * Se tienen que proponer por lo menos cuatro reglas si - entones, para la inferencia difusa (se tiene que utilizar por lo menos una vez cada conjunto difuso propuesto en su semántica)
+%
+%% Parametros elegidos
+% Se opto por agregar 5 funciones de membresia a cada entrada
+% 
+%% Imagen de la entrada 1
+% 
+% << input1.png >>
+% 
+
+
+   
+    
+    
+    
+    
+
